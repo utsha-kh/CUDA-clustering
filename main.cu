bbfd66f03ddfd7ea57014dab9326222359f71232
utsha-kh <utsha.kh@gmail.com>
2020-04-09 14:17:39 -0500

    First commit, nothing special.

---
+++ main.cu
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <sstream>
+#include <vector>
+
+
+int main (int argc, char** argv) {
+
+    printf("Hello new PC!");
+    return 0;
+
+}
