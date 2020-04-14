1098036ca6e4a2af89ebdd4146c0cafe3c030cd7
utsha-kh <utsha.kh@gmail.com>
2020-04-14 01:57:52 -0500

    Implemented non-parallel k-mean clustering, not tested

--- kmeans.cpp
+++ kmeans.cpp
@@ -11,9 +11,11 @@ private:
     int k;  // number of groups to cluster
     vector<vector<int>> x;  // input data points
     vector<int> whichSet;  // stores info that which set a vector belong to
-    vector<int> u; // centers of each of k sets
-    set<vector<int>> S; // k different sets of vectors
+    vector<vector<int>> u; // centers of each of k sets
+    //vector<set<vector<int>>> S; // k different sets of vectors
+    bool converged = false;
 
+    // return L2 distance between two points
     int getDistance(vector<int> x1, vector<int> x2){
         int d = 0;
         for(int i = 0; i < x1.size; i++){
@@ -22,7 +24,55 @@ private:
         return d; 
     }
 
+    vector<int> addVector(vector<int> x1, vector<int> x2){
+        vector<int> retVector;
+        retVector.resize(x1.size);
+        for(int i = 0; i < x1.size; i++){
+            retVector[i] = x1[i] + x2[i];
+        }
+        return retVector;
+    }
 
+    // initialize each center values u_i to a randomly chosen data point
+    void initCenters(){
+        // Each center u[i] should be a random data point x[j], but 
+        // generating a non-repeated random number isn't straightforward
+        // so I'll do it later
+        for(int i = 0; i < k; i++){
+            u[i] = x[i];
+        } 
+    }
+
+    // Assign each data point x_i to the closest center
+    void assignDataPoints(){
+        for(int i = 0; i < n; i++){
+            //S[whichSet[i]].erase(x[i]);
+            int closest = 0;
+            int minDistance = INTMAX_MAX;
+            for(int j = 0; j < k; j++){
+                if(getDistance(x[i], u[j]) < minDistance){
+                    closest = j;
+                    minDistance = getDistance(x[i], u[j]);
+                }
+            }
+            whichSet[i] = closest;
+            //S[whichSet[i]].insert(x[i]);
+        }
+    }
+
+    // Update each center u_i to average of all points who belong to that set
+    void updateCenters(){
+        for(int i = 0; i < k; i++){
+            vector<int> sum;
+            sum.resize(d);  // a d-dimensional vector
+            for(int j = 0; j < n; j++){
+                if(whichSet[j] == i){
+                    sum = addVector(sum, x[j]);
+                }
+            }
+            u[i] = sum;
+        }
+    }
 
 public:
     kMeans(int n, int d, int k, vector<vector<int>> x){
@@ -30,13 +80,17 @@ public:
         this->d = d;
         this->k = k;
         this->x = x;
-        this->whichSet = new vector<int>(n);
-        this->u = new vector<int>(k);
-        this->S = new set<vector<int>>(k);
+        this->whichSet.resize(n);
+        this->u.resize(k);
+        //this->S.resize(k);
     }
 
     void kMeansClustering(){
-        return;
+        initCenters();
+        while(!converged){
+            assignDataPoints();
+            updateCenters();
+        }
     }
 
 }
