100089aa32f7855d1c9ce7da3cb845d28b45000d
Utsha Khondkar <utsha.kh@gmail.com>
2020-04-28 20:07:19 -0500

    added code to measure execution time

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <numeric>
 #include <sstream>
 #include <vector>
+#include <chrono>
 #include "kmeans.h"
 #include "parser.h"
 
@@ -14,11 +15,18 @@ int main (int argc, char** argv) {
     //char *filename = argv[0];
     Parser parser(filename);
 
-    KMeans module(parser.rows, parser.cols, 5, parser.data);
+    KMeans module(parser.rows, parser.cols, 3, parser.data);
 
+    auto t1 = std::chrono::high_resolution_clock::now();
     module.kMeansClustering();
+    auto t2 = std::chrono::high_resolution_clock::now();
+
     //parser.print();
     parser.toCSV("result.csv", module.getData(), module.getLabel());
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+    std::cout << "Execution Time: " << duration / 1000 << " [ms]" << std::endl;
+    
     return 0;
 
 }
